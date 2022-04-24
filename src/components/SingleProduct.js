670327336e2c773070adf82dd3c68ef1e4f31f73
marisa <marisaonthego@gmail.com>
2022-04-24 10:36:18 -0600

    AllProducts, SingleProduct

---
+++ src/components/SingleProduct.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const SingleProduct = ({ product }) => {
+
+    return (
+          <div
+            key={product.id}
+            id='single-product'
+            style={{ border: "1px solid black" }}>
+            <h4><u>Name:</u>{product.name}</h4>
+            <h4><u>Description:</u>{product.description}</h4>
+            <h4><u>Price: </u>{product.price}</h4>
+            <img src="${product.imagurl}" alt="photo of ${product.name} the ${product.name}"/>
+            <h4><u>In stock?</u>{product.inStock}</h4>   
+          </div>
+    )
+        
+};
+
+export default SingleProduct;
+
