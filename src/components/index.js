a2885cd80da1c04cd10525dd0a4636245fb8828b
marisa <marisaonthego@gmail.com>
2022-04-27 14:09:17 -0600

    Front SingleOrder

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,5 @@ export { default as Login } from './Login';
 export { default as Register } from './Register';
 export { default as AllProducts } from './AllProducts';
 export { default as SingleProduct } from './SingleProduct';
+export { default as SingleUser } from './SingleUser';
+export { default as SingleOrder } from './SingleOrder';
