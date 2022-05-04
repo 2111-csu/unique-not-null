ba907fd7e703cbb08d9f229b5d472bceb2b7d838
Nick <nick.mullen91@gmail.com>
2022-05-03 21:15:17 -0600

    local storage working for guest cart, need to see how to create an order from localstorage next

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -9,18 +9,26 @@ const Cart = ({ myCart, setMyCart, token }) => {
 
 
   const getCart = async () => {
-    const userCart = await callApi({
-      url: '/api/orders/cart',
-      token,
-      method: 'GET'
-    });
-    console.log('userCart', userCart.data);
-    setMyCart(userCart.data[0]);
+    try {
+      const userCart = await callApi({
+        url: '/api/orders/cart',
+        token,
+        method: 'GET'
+      });
+      if (userCart) {
+        console.log('userCart', userCart.data);
+        setMyCart(userCart.data[0]);
+      }
+      
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+    
   }
 
-  useEffect(() => getCart(), []); 
+  // useEffect(() => getCart(), []); 
 
-  /* new stuff */
   const handleEditQuantity = async (event, productId) => {
     event.preventDefault();
     try {
@@ -52,43 +60,41 @@ const Cart = ({ myCart, setMyCart, token }) => {
         throw error
       }
     };
-   
+  
   return (
     <>
-      <div key={myCart.id} id="single-order" style={{ border: "1px solid black", margin:"20px"}}>
-        <h4><u>Order id: </u>{myCart.id}</h4>
-        <h4><u>Date placed: </u>{myCart.datePlaced}</h4>
-      </div>
+      {myCart? 
+      <>
+        <div key={myCart.id} id="single-order" style={{ border: "1px solid black", margin:"20px"}}>
+          <h4><u>Order id: </u>{myCart.id}</h4>
+          <h4><u>Date placed: </u>{myCart.datePlaced}</h4>
+        </div>
 
-      {myCart.products && myCart.products.map((product) => {
-        return (
-          <div key={product.id}>
-            <h4>product id: {product.id}</h4>
-            <h4>product name: {product.name}</h4>
-            <h4>product description: {product.description}</h4>
-            <h4>price: {product.price}</h4>
+        {myCart.products && myCart.products.map((product) => {
+          return (
+            <div key={product.id}>
+              <h4>product id: {product.id}</h4>
+              <h4>product name: {product.name}</h4>
+              <h4>product description: {product.description}</h4>
+              <h4>price: {product.price}</h4>
 
-            <input type='number' id='quantity-input' name='quantity' min='0'max='10'
-            placeholder='Quantity' value={quantity} onChange={(event) => setQuantity(event.target.value)}/>
+              <input type='number' id='quantity-input' name='quantity' min='0'max='10'
+              placeholder='Quantity' value={quantity} onChange={(event) => setQuantity(event.target.value)}/>
 
-            <h4>quantity: {product.quantity}</h4>
+              <h4>quantity: {product.quantity}</h4>
 
-          {/*new buttons*/}
-            <button type="submit"className="button"
+              <button type="submit"className="button"
+              onClick={(e) => handleEditQuantity(e, product.id)}>Change Quantity</button>
 
-            onClick={(e) => handleEditQuantity(e, product.id)}>Change Quantity</button>
+              <button type="submit"className="button"
+              onClick={(e) => handleRemoveProduct(e, product.id)}>Remove Product</button>  
 
-            <button type="submit"className="button"
-            onClick={(e) => handleRemoveProduct(e, product.id)}>Remove Product</button>  
-
-          </div>
-        );
-      })}
+            </div>
+          );
+        })}
+      </> : <h1>Nothing to show here</h1> }
     </>
   );
 };
 
 export default Cart;
-
-/* 
-*/
