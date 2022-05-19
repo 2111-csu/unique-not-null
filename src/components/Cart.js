b5ff6d655c598998468732416fd2dba2ee5ae62f
Nick <nick.mullen91@gmail.com>
2022-05-19 15:59:59 -0600

    Fixed error in cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -25,6 +25,7 @@ const Cart = ({ myCart, setMyCart, token, loggedIn, guestCart, setGuestCart  }) 
             }
           });
           localStorage.setItem('guestCart', JSON.stringify({products: []}));
+          setGuestCart()
         } catch (error) {
           console.log(error);
           throw error;
